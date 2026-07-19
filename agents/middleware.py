56e9cabb910e1f7d40c9adaa30e304a409b4f6f9
Prashanth <prashanthsamkumar@gmail.com>
2026-07-19 22:49:08 +0530

    feat: add agent middlewares

---
+++ agents/middleware.py
@@ -0,0 +1,32 @@
+from langchain.agents.middleware import (
+    ModelFallbackMiddleware,
+    SummarizationMiddleware,
+    ToolCallLimitMiddleware,
+    ContextEditingMiddleware,
+    ClearToolUsesEdit
+)
+
+def get_middlewares() -> list:
+    return [
+        ModelFallbackMiddleware(
+            "groq:llama-3.1-8b-instant",       
+            "google_genai:gemini-2.0-flash"
+        ),
+        SummarizationMiddleware(
+            model="groq:llama-3.1-8b-instant",
+            trigger=("tokens", 4000),
+            keep=("messages", 20),
+        ),
+        ToolCallLimitMiddleware(
+            thread_limit=20, 
+            run_limit=10
+        ),
+        ContextEditingMiddleware(
+            edits=[
+                ClearToolUsesEdit(
+                    trigger=10000,
+                    keep=3,
+                ),
+            ]
+        )
+    ]
